a7214764871727e10ede6d5c78c245a47ac6b117
Abhilash-03 <abhilashjha335@gmail.com>
2026-04-08 21:46:25 +0530

    refactor: update Gemini GenAI integration and dependencies

--- controllers/genai.js
+++ controllers/genai.js
@@ -1,13 +1,17 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai');
+const { GoogleGenAI } = require("@google/genai");
 
-const genAI = new GoogleGenerativeAI(process.env.GENAI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const genAI = new GoogleGenAI({ apiKey: process.env.GENAI_API_KEY });
 
 const genai = async(req, res) => {
     try {
         const {prompt} = req.body;
-        const result = await model.generateContent(prompt);
-        res.status(200).send(result.response.text());
+        // res.setHeader('Content-Type', 'text');
+        const result = await genAI.models.generateContent({
+            model: "gemini-2.5-flash-lite",
+            contents: prompt,
+        });
+        console.log("Response genai: ", result.text);
+        res.status(200).send(result.text);
     } catch (error) {
         res.status(400).send('Having an issue while generating :(')
     }
